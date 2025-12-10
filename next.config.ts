d782d56bd8f857fa146d8ea1ffde9320e0883ceb
unusali1 <mdunusali1906@gmail.com>
2025-12-10 19:07:29 +0600

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["fakestoreapi.com"], // <-- whitelist this domain
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+        port: "",          // leave empty if default
+        pathname: "/**",   // allow all paths from this domain
+      },
+    ],
   },
 };
 
